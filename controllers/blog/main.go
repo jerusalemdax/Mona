30767b92d76d9a5f95561b23dd8814b79da79fb1
jerusalemdax <jerusalemdax@gmail.com>
2015-10-09 21:26:33 +0800

    增加404页面

--- controllers/blog/main.go
+++ controllers/blog/main.go
@@ -1,6 +1,11 @@
 package blog
 
-import "github.com/jerusalemdax/Mona/models"
+import (
+	"strconv"
+	"strings"
+
+	"github.com/jerusalemdax/Mona/models"
+)
 
 type MainController struct {
 	baseController
@@ -19,4 +24,45 @@ func (this *MainController) Index() {
 	this.Data["pagebar"] = models.NewPager(int64(this.page), int64(count), int64(this.pagesize), "/index%d.html").ToString()
 	this.setHeadMetas()
 	this.display("index")
+}
+
+//留404页面
+func (this *MainController) Go404() {
+	this.setHeadMetas("Sorry 404页面没找到")
+	this.display("404")
+}
+
+//文章显示
+func (this *MainController) Show() {
+	var (
+		post *models.Post = new(models.Post)
+		err  error
+	)
+	urlname := this.Ctx.Input.Param(":urlname")
+	if urlname != "" {
+		post.Urlname = urlname
+		err = post.Read("urlname")
+	} else {
+		id, _ := strconv.Atoi(this.Ctx.Input.Param(":id"))
+		post.Id = int64(id)
+		err = post.Read()
+	}
+	if err != nil || post.Status != 0 {
+		this.Redirect("/404.html", 302)
+	}
+	post.Views++
+	post.Update("Views")
+	models.Cache.Delete("hotblog")
+	post.Content = strings.Replace(post.Content, "_ueditor_page_break_tag_", "", -1)
+	pre, next := post.GetPreAndNext()
+	this.Data["post"] = post
+	this.Data["pre"] = pre
+	this.Data["next"] = next
+	this.Data["smalltitle"] = "文章内容"
+	if urlname == "about.html" {
+		this.Data["smalltitle"] = "关于我"
+	}
+
+	this.setHeadMetas(post.Title, strings.Trim(post.Tags, ","), post.Title)
+	this.display("article")
 }
--- routers/router.go
+++ routers/router.go
@@ -8,6 +8,7 @@ import (
 func init() {
 	//前台路由
 	beego.Router("/", &blog.MainController{}, "*:Index")
-	//beego.Router("/404.html", &blog.MainController{}, "*:Go404")
+	beego.Router("/404.html", &blog.MainController{}, "*:Go404")
+	beego.Router("/:urlname(.+)", &blog.MainController{}, "*:Show")
 	//beego.Router("/index:page:int.html", &blog.MainController{}, "*:Index")
 }
